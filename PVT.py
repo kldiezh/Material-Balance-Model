05e3704cc0c2c01ed60410a6739ebb7751be3a83
Kelly Díez <75863468+kldiezh@users.noreply.github.com>
2024-11-23 16:40:19 -0500

    Update PVT.py

--- PVT.py
+++ PVT.py
@@ -6,7 +6,7 @@ import pandas as pd
 P = np.arange(Pi,2180,-20) # Pressure array
 
 # Init the pressures
-# Using numpy arrays
+# Using numpy arrays for this
 
 P_UR = np.array([]) # Pressures above Pb.
 P_SR = np.array([]) # Presssure below Pb.
